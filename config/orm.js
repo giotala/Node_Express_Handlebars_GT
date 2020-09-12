6782d03ea2fc164e59b04afe1892a4ab68236fff
Gio Talabong <gio.talabong@gmail.com>
2020-09-11 21:52:56 -0700

    clean up

--- config/orm.js
+++ config/orm.js
@@ -56,7 +56,7 @@ var orm = {
       cb(result);
     });
   },
-  // An example of objColVals would be {name: panther, sleepy: true}
+  
   update: function(table, objColVals, condition, cb) {
     var queryString = "UPDATE " + table;
 
@@ -75,5 +75,4 @@ var orm = {
   }
 };
 
-// Export the orm object for the model (cat.js).
 module.exports = orm;
